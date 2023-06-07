d5a082a242a1802d2fe0c8f5ab6f4c6beba936ac
aristote <aryaristote@gmail.com>
2023-06-07 12:08:45 +0200

    controller setup

--- public/index.php
+++ public/index.php
@@ -2,12 +2,14 @@
 
 require_once __DIR__ ."/../vendor/autoload.php";
 
+use App\controllers\SiteController;
 use App\core\Application;
+
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', 'home');
-
-$app->router->get('/contact', "Contact");
+$app->router->get('/', [SiteController::class, 'home']);
+$app->router->get('/contact', [SiteController::class, 'contact']);
+$app->router->post('/contact', [SiteController::class, "handleContact"]);
 
 $app->run();
 
